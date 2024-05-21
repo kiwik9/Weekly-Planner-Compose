2cc4e18fd0fb82a7786ad0fc2a645d17ce1878ce
Julio Perez <jperezr096@gmail.com>
2024-05-21 09:21:26 -0500

    Modify datastore

--- data/src/main/java/io/kiwik/data/datastore/UserDataStore.kt
+++ data/src/main/java/io/kiwik/data/datastore/UserDataStore.kt
@@ -1,9 +1,6 @@
 package io.kiwik.data.datastore
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
