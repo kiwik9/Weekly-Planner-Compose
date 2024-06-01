e20ecf506355a1149bb9095ac2de63b8a1862fbf
Julio Perez <jperezr096@gmail.com>
2024-05-31 20:40:20 -0500

    modify

--- app/src/main/java/io/kiwik/weeklyplanner/features/home/screen/taks/ListTaskView.kt
+++ app/src/main/java/io/kiwik/weeklyplanner/features/home/screen/taks/ListTaskView.kt
@@ -1,25 +1,19 @@
 package io.kiwik.weeklyplanner.features.home.screen.taks
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.animation.core.Easing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.kiwik.domain.model.Task
 import io.kiwik.ui.components.AddTaskItem
 import io.kiwik.ui.components.TaskItem
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ListTaskView(
     modifier: Modifier,
