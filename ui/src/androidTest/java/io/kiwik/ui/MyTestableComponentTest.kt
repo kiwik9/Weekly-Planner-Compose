eece0ee8532dbc81aeeedf78d75190b01bed6b1c
Julio Perez <jperezr096@gmail.com>
2025-07-08 18:05:57 -0500

    validate

--- ui/src/androidTest/java/io/kiwik/ui/MyTestableComponentTest.kt
+++ ui/src/androidTest/java/io/kiwik/ui/MyTestableComponentTest.kt
@@ -53,51 +53,6 @@ class MyTestableComponentTest {
                 enableFirstButton = false
             )
         }
-
-        //FINDER
-        rule.onNodeWithText("component", ignoreCase = true) // Busca por contenido del texto
-        rule.onNodeWithTag("component1") // Busca por tag Modifier.testTag("")
-        rule.onNodeWithContentDescription("superImage") // Busca por description de una imagen o icono
-
-        rule.onAllNodesWithText("a") // Buscar por text pero todos los elementos
-        rule.onAllNodesWithTag("component1") // Buscar por tag pero todos los elementos
-        rule.onAllNodesWithContentDescription("visualIcon") /// Buscar por description pero todos los elementos
-
-
-        //ACTIONS
-        rule.onNodeWithText("aris", ignoreCase = true).performClick()
-        rule.onAllNodesWithText("a").onFirst().performClick()
-        rule.onNodeWithText("aris").performTouchInput {
-            longClick()
-            doubleClick()
-            swipeDown()
-            swipeUp()
-            swipeLeft()
-            swipeRight()
-        }
-        rule.onNodeWithText("aris").performScrollTo().performClick().performTextInput("")
-        rule.onNodeWithText("aris").performImeAction()
-        rule.onNodeWithText("aris").performTextClearance()
-        rule.onNodeWithText("aris").performTextInput("adwdawdawdawd")
-        rule.onNodeWithText("aris").performTextReplacement("dawdaw")
-
-        //ASSERTIONS
-        rule.onNodeWithText("aris").assertExists()
-        rule.onNodeWithText("aris").assertDoesNotExist()
-        rule.onNodeWithText("aris").assertContentDescriptionEquals("EADWAD")
-        rule.onNodeWithText("aris").assertContentDescriptionContains("EADWAD")
-        rule.onNodeWithText("aris").assertIsDisplayed()
-        rule.onNodeWithText("aris").assertIsNotDisplayed()
-        rule.onNodeWithText("aris").assertIsEnabled()
-        rule.onNodeWithText("aris").assertIsNotEnabled()
-        rule.onNodeWithText("aris").assertIsSelected()
-        rule.onNodeWithText("aris").assertIsNotSelected()
-        rule.onNodeWithText("aris").assertIsFocused()
-        rule.onNodeWithText("aris").assertIsNotFocused()
-        rule.onNodeWithText("aris").assertIsOn()
-        rule.onNodeWithText("aris").assertIsOff()
-        rule.onNodeWithText("aris").assertTextEquals("")
-        rule.onNodeWithText("aris").assertTextContains("Aris")
     }
 
     @Test
